70ebd3655b1e6b7b7deb99f084853995bc100e22
palanibsm <senthil2world@gmail.com>
2020-03-09 21:15:23 +1300

    1

---
+++ terraform-aws-rds-oracle/locals.tf
@@ -0,0 +1,14 @@
+# -----------------------------------
+# locals variable file
+# -----------------------------------
+
+locals {
+    security_aliases = jsondecode(data.aws_ssm_parameter.security_aliases.value)
+    environment      = var.environment == "" ? data.aws_ssm_parameter.environment.value : var.environment
+
+    tags = {
+        LinkedAccountId = data.aws_ssm_parameter.aws_account_id.value      # AWS Account Id
+        projectname     = var.projectname
+        environment     = local.environment
+    }
+}
